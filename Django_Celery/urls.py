4de1399aff4ec2dcc7f4dc48bd2ecd2a0d1fb115
islombeknematov <nematov.islom.dev@gmail.com>
2022-02-04 15:36:40 +0500

    added views, urls, html

--- Django_Celery/urls.py
+++ Django_Celery/urls.py
@@ -2,9 +2,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+from users.views import SignUpFormView, SignInFormView, sign_out, AccountTemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', SignUpFormView.as_view(), name='home'),
+    path('sign-in/', SignInFormView.as_view(), name='sign-in'),
+    path('sign-out/', sign_out, name='sign-out'),
+    path('account/', AccountTemplateView.as_view(), name='sign-out'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_URL)
--- users/views.py
+++ users/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.contrib.auth import logout
+from django.shortcuts import redirect, reverse
+from django.views import generic
+from django.views.generic import TemplateView, FormView
 
-# Create your views here.
+from users.forms import UserForm, AuthForm
+
+
+class SignUpFormView(FormView):
+    template_name = 'sign_up.html'
+    form_class = UserForm
+    success_url = '/account/'
+
+
+class SignInFormView(FormView):
+    template_name = 'sign_in.html'
+    form_class = AuthForm
+    success_url = '/account/'
+
+
+def sign_out(request):
+    logout(request)
+    return redirect(reverse('sign-in'))
+
+
+class AccountTemplateView(TemplateView):
+    template_name = 'account.html'
+
+
+
